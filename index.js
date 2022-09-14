285339dbae809332428d805381e13df04e51a3bd
Tahir <tahir@shopiniworld.com>
2022-09-14 09:30:44 +0400

    cors update

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const errorMiddleware = require("./middlewares/error.middleware");
 const validators = require("./middlewares/validators");
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
 const port = process.env.PORT;
+app.use(cors({ allowedOrigin: ["http://localhost:3000/"] }))
 app.use(express.json());
 console.log(`Node environment: ${process.env.NODE_ENV}`);
 app.listen(port, () => {
